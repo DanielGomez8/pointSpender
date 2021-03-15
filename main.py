c30b8cb8712ccd1e6c14a65cb5f328616feebc9a
Daniel Gomez <danielgomez0024@gmail.com>
2021-03-14 19:03:42 -0600

    commented test cases

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ u = user.User()
 for testing purposes
 '''
 #u.add_transaction({"payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z"})
-u.add_transaction({"payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z"})
-u.add_transaction({"payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z"})
-u.add_transaction({"payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z"})
-u.add_transaction({"payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z"})
+#u.add_transaction({"payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z"})
+#u.add_transaction({"payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z"})
+#u.add_transaction({"payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z"})
+#u.add_transaction({"payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z"})
 
 
 '''
